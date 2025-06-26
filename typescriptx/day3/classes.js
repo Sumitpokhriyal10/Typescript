ebae15c50df9f5ba26fe4541e25b7f5d357ee0ca
Sumit Pokhriyal <sumitpokhriyalonezero@gmail.com>
2025-06-26 09:41:38 -0700

    classes and generics

--- typescriptx/day3/classes.js
+++ typescriptx/day3/classes.js
@@ -1,20 +1,23 @@
 "use strict";
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    //how to make function inside class 
-    greet() {
-        console.log(`hi ${this.name}`);
-    }
-}
+//class Person{  //CLASS IS THE BLUE PRINT OF OBJECT
+// [it is inside the class that how will our object look]
+//  name:string;
+// age:number;
+// constructor(name:string,age:number){
+//    this.name=name;
+//    this.age=age;
+// }
+//how to make function inside class 
+//  greet():void{
+//      console.log(`hi ${this.name}`);
+// }
+//}
 //object create
-const obj1 = new Person("rohit", 10);
-const obj3 = new Person("ni", 10);
-//console.log(obj1)
-console.log(obj1.name);
-obj1.greet();
+//const obj1 = new Person("rohit",10);
+//const obj3 = new Person("ni",10);
+//console.log( typeof obj1)
+//console.log(obj1.name)
+//obj1.greet();
 //EXTENDS KEYWORDS
 //interface human{
 //name:string,
@@ -25,7 +28,7 @@ obj1.greet();
 //id:number
 //}
 //interface bankemp extends human {
-//   salary:string,
+//   salary1:string,
 //   postion:string
 //}
 //const obj5:Teacher={
@@ -34,3 +37,37 @@ obj1.greet();
 //  salary:"chilar",
 //  id:55
 //}
+class Customer {
+    constructor(name, age, balance) {
+        this.name = name;
+        this.age = age;
+        this.balance = balance;
+    }
+    meet() {
+        this.age = this.age + 10;
+        return this.age;
+    }
+}
+const p1 = new Customer("Deepak", 20, 500);
+console.log(p1.name); // ✅ Accessing public property
+console.log(p1.meet()); // ✅ Calling method that returns updated age
+class bankemployee extends Customer {
+    constructor(salary, name, age, balance) {
+        super(name, age, balance);
+        this.salary = salary;
+    }
+    greet() {
+        console.log(this.balance);
+    }
+}
+const E1 = new bankemployee(10, "sumit", 20, 300);
+console.log(E1);
+//generics:template
+function value(a) {
+    return a;
+}
+value(10);
+const obj80 = {
+    name: "rohit",
+    adhar: 45
+};
--- typescriptx/day3/classes.ts
+++ typescriptx/day3/classes.ts
@@ -1,23 +1,24 @@
-class Person{
-    name:string;
-    age:number;
-    constructor(name:string,age:number){
-        this.name=name;
-        this.age=age;
-    }
+//class Person{  //CLASS IS THE BLUE PRINT OF OBJECT
+// [it is inside the class that how will our object look]
+  //  name:string;
+   // age:number;
+   // constructor(name:string,age:number){
+    //    this.name=name;
+    //    this.age=age;
+   // }
     //how to make function inside class 
-    greet():void{
-        console.log(`hi ${this.name}`);
-    }
-}
+  //  greet():void{
+  //      console.log(`hi ${this.name}`);
+  // }
+//}
 
 //object create
-const obj1 = new Person("rohit",10);
-const obj3 = new Person("ni",10);
-//console.log(obj1)
+//const obj1 = new Person("rohit",10);
+//const obj3 = new Person("ni",10);
+//console.log( typeof obj1)
 
-console.log(obj1.name)
-obj1.greet();
+//console.log(obj1.name)
+//obj1.greet();
 
 
 //EXTENDS KEYWORDS
@@ -34,7 +35,7 @@ obj1.greet();
 
 
 //interface bankemp extends human {
- //   salary:string,
+ //   salary1:string,
  //   postion:string
 //}
 
@@ -44,3 +45,56 @@ obj1.greet();
   //  salary:"chilar",
   //  id:55
 //}
+
+
+class Customer {
+  public name: string;
+  private age: number;
+  protected balance: number;
+
+  constructor(name: string, age: number, balance: number) {
+    this.name = name;
+    this.age = age;
+    this.balance = balance;
+  }
+
+   public meet(): number {
+    this.age = this.age + 10;
+    return this.age;
+  }
+}
+
+const p1 = new Customer("Deepak", 20, 500);
+console.log(p1.name);       // ✅ Accessing public property
+console.log(p1.meet());     // ✅ Calling method that returns updated age
+
+
+class bankemployee extends Customer{
+    salary:number;
+    constructor(salary:number,name: string, age: number,balance: number){
+        super(name,age,balance);
+        this.salary=salary;
+    }
+    greet():void{
+        console.log(this.balance);
+    }
+}
+const E1=new bankemployee(10,"sumit",20,300);
+console.log(E1);
+
+//generics:template
+function value<T>(a:T):T{
+    return a;
+}
+
+value(10);
+
+interface admin<T>{
+    name:string,
+    adhar:T
+}
+
+const obj80:admin<number>={
+  name:"rohit",
+  adhar:45
+}
